c3af5baa61b92b27b8a5e83a01e2a9505f939141
MSI\banba <banban.n.namchan@gmail.com>
2021-07-06 14:32:36 +0900

    nanbaクラスを追加

---
+++ HelloWorld_nannba/HelloWorld_nannba/nanba.cpp
@@ -0,0 +1,7 @@
+#include"nanba.h"
+#include<stdio.h>
+
+void ShowName()
+{
+	printf("“ï”g‘ñ‘\n");
+}
---
+++ HelloWorld_nannba/HelloWorld_nannba/nanba.h
@@ -0,0 +1,10 @@
+#ifndef NANBA_H
+#define NANBA_H
+
+class nanba
+{
+public:
+	void ShowName();
+};
+#endif // !NANBA_H
+
